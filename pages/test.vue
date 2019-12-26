03ca90430a4c880b98b9a3363806c1495f60bd22
fkkmemi <fkkmemi@gmail.com>
2019-12-26 23:32:01 +0900

    10 스크롤 페이징 맛보기

--- pages/test.vue
+++ pages/test.vue
@@ -1,3 +1,45 @@
 <template>
-  <v-card>hi?</v-card>
+  <v-container>
+    <v-list three-line>
+      <v-list-item v-for="(item, i) in items" :key="i">
+        <v-list-item-content>
+          <v-list-item-title>{{ i + ' ' + item.title }}</v-list-item-title>
+          <v-list-item-subtitle>{{ item.subtitle }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ item.date }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-card v-intersect="onIntersect" class="text-center" flat>
+      <v-btn :loading="loading">
+        More
+      </v-btn>
+    </v-card>
+  </v-container>
 </template>
+<script>
+import delay from 'delay'
+
+export default {
+  data () {
+    return {
+      items: [],
+      loading: false
+    }
+  },
+  methods: {
+    async onIntersect (entries, observer, isIntersecting) {
+      // console.log(entries, observer, isIntersecting)
+      this.loading = true
+      await delay(3000)
+      for (let i = 0; i < 10; i++) {
+        this.items.push({
+          title: Math.random(),
+          subtitle: Math.random(),
+          date: new Date().toLocaleString()
+        })
+      }
+      this.loading = false
+    }
+  }
+}
+</script>
